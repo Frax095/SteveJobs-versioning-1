a6d2b9b33c840b326c558055d5cb3ee1578be801
frax095 <frax095@gmail.com>
2019-04-12 11:39:59 +0200

    StructureServerBase

---
+++ app.js
@@ -0,0 +1,10 @@
+var express = require('express');
+var bodyParser = require('body-parser');
+var app = express();
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+
+
+
+app.listen(3000);
+module.exports = app;
