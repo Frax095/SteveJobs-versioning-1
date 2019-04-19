647b3e2a2c2b4f02754d1c7b33297ee8a0119764
frax095 <frax095@gmail.com>
2019-04-19 10:15:24 +0200

    cartella controller

--- routes/users.js
+++ routes/users.js
@@ -1,19 +1,19 @@
 var express = require('express');
 var router = express.Router();
-var fake = require('userFunction');
+var User = require('../controller/userFunction');
 
 router.delete('/:id', validation, function(req, res){
     var id = parseInt(req.params.id);
-    res.json(fake.deleteUserById(id));
+    res.json(User.deleteUserById(id));
 })
 
 router.get('/:id', function(req, res){
     var id = parseInt(req.params.id);
-    res.json(fake.getUserById(id));
+    res.json(User.getUserById(id));
 })
 
 router.post('/', function(req, res){
-    fake.postUser(req.body.name, req.body.surname, req.body.email, req.body.born, req.body.gender)
+    User.postUser(req.body.name, req.body.surname, req.body.email, req.body.born, req.body.gender)
     res.status(201).json();
 })
 
