242ecb3a275ff85cfe31ad18667d3c48e463d04b
Andrea <andrea.ardenti@gmail.com>
2019-05-14 21:46:57 +0200

    test and code coverage

--- app.js
+++ app.js
@@ -6,11 +6,12 @@ app.use(bodyParser.urlencoded({extended: true}));
 
 const users = require('./routes/userApi');
 app.use('/users', users);
+
 // collegamento al database
 const host = 'localhost';
 const dbName = 'stevejobs-versioning-1';
 const mongoose = require('mongoose');
-mongoose.connect(`mongodb://${host}/${dbName}`);
+mongoose.connect('mongodb://' + host + '/' + dbName, {useNewUrlParser: true});
 
 const db = mongoose.connection;
 db.on('error', function() {
@@ -23,5 +24,18 @@ db.once('open', function() {
 // uso dello schema
 const User = require('./models/userSchema');
 
-app.listen(3001);
+app.use(function(req, res, next) {
+    const err = new Error('Not Found');
+    err.status = 404;
+    next(err);
+});
+
+app.use(function(err, req, res, next) {
+    res.status(err.status || 500)
+        .json({message: err.message, error: err});
+});
+
+
+app.listen(3002);
 module.exports = app;
+
